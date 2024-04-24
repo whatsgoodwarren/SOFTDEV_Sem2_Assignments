0c9224ffb910216633bea01c7a114f3795d35a06
Caleb Nzao <nzaocal000@warren.k12.in.us>
2024-04-24 10:34:21 -0400

    commit me

--- 04_Try Catch Finally/Assignment/src/App.java
+++ 04_Try Catch Finally/Assignment/src/App.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int firstNumber = input.nextInt();
+        System.out.println("Enter another number: ");
+        int secondNumber = 0;
+        
+        
+        int quotient = 0;
+        String printQuotient = " ";
+        try {
+            quotient = firstNumber / secondNumber;
+            printQuotient = " " + quotient;
+        } catch (ArithmeticException ex) {
+            System.out.println(ex.getMessage());
+            printQuotient = "Undefined";
+        }
     }
 }
